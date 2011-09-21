9a46a7cc0d934b09135e2c318891319ea782e843
Greg Edwards <greg.edwards@openlogic.com>
2011-09-21 13:58:23 -0600

    Fix for rake -T not working

--- lib/couch-migrate.rb
+++ lib/couch-migrate.rb
@@ -1,2 +1,4 @@
 require_relative 'couch_migrate_railtie.rb' if defined?(Rails)
-Dir["#{File.dirname(__FILE__)}/couch_migrate/**/*.rb"].each {|f| require f}
+list = Dir["#{File.dirname(__FILE__)}/couch_migrate/**/*.rb"]
+skip = Dir["#{File.dirname(__FILE__)}/couch_migrate/couchrest_model/**/*.rb"]
+(list - skip).each {|f| require f}
