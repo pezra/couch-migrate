edcdef400adcface2ed6b6fbc97054d6d1fd18f8
Greg Edwards <greg.edwards@openlogic.com>
2011-09-21 13:29:56 -0600

    couchrest_model integration improved: can now handle views correctly

--- lib/couch_migrate/couchrest_model/extend.rb
+++ lib/couch_migrate/couchrest_model/extend.rb
@@ -1,15 +1,17 @@
 # use "use_couchrest_model" in the DSL or
 # include this module in a migration to use CouchRest::Model
-if defined?(CouchRest::CouchRest::Model)
-  module CouchRest
+if defined?(::CouchRest::Model)
+  module ::CouchRest
     module Model
       class Base
-        before_save :rewrite_type_for_migration_classes
+        class << self
+          alias to_s_original to_s
 
-        def rewrite_type_for_migration_classes
-          class_name = self.class.to_s.sub(/CouchMigrate::BaseExecuter::Namespaced::/, '')
-          self['type'] = class_name
+          def to_s
+            to_s_original.sub(/CouchMigrate::BaseExecuter::Namespaced::/, '')
+          end
         end
+
       end
     end
   end
