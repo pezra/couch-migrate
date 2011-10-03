bde585364e07095749da2bfcfdf1cf7e5f976c4d
Greg Edwards <greg.edwards@openlogic.com>
2011-10-03 13:11:07 -0600

    base_migrater_spec.rb refactor

--- spec/couch_migrate/base_migrater_spec.rb
+++ spec/couch_migrate/base_migrater_spec.rb
@@ -17,28 +17,21 @@ describe "BaseMigrater", "#migrate" do
   context "interacting with the file system" do
     include FakeFS
 
-    before do 
+    let(:path){ Pathname.new("/tmp/dm/migrate") }
+
+    before(:all) do
       FakeFS.activate!
-    end
+      path.mkpath
 
-    def self.migration(name, file_name, content="up do end")
-      let!(name) {
-        file_name = send(file_name) if Symbol === file_name
-        (path + file_name).tap do |f|
-          File.open(f, 'w') {|f| f << content}
-        end
-      }
-    end
-    
-    let(:path){ Pathname.new("/tmp/dm/migrate").tap{|p| p.mkpath} }
+      # create migration files
+      [
+        [file_name_1, "up do end" ],
+        [file_name_2, "up do raise '2 simulated failure' end" ],
+        [file_name_3, "up do end" ],
+      ].each do |file_name, content|
+        File.open(path+file_name, 'w') {|f| f << content}
+      end
 
-    migration(:file_1, :file_name_1)
-    migration(:file_2, :file_name_2, "up do raise '2 simulated failure' end" )
-    migration(:file_3, :file_name_3)
-    migration(:editor_backup_file, ".#1_migration_a.rb")
-
-    after do
-      FakeFS.deactivate!
     end
 
     it "reads the db/migrations directory to generate a list of potential migrations to run" do
@@ -48,9 +41,15 @@ describe "BaseMigrater", "#migrate" do
 
     it "stops when a migration file exits with an error, not processing any subsequent files afterwards" do
       subject.directory(path).pending_migrations.should == [file_name_1, file_name_2, file_name_3]
+
       subject.migrate(:quiet, :up).should == {success: [file_name_1], failed: [file_name_2]}
+
       subject.failed_migration.should == file_name_2
       subject.pending_migrations.should == [file_name_2, file_name_3]
+    end
+
+    after(:all) do
+      FakeFS.deactivate!
     end
 
   end
@@ -90,8 +89,14 @@ describe "BaseMigrater", "sorting and filtering" do
   end
 
   it "ignores files that do not have follow the migrationOrder_migration_title format" do
-    subject.raw_migrations(['2_name.rb','a_bad_name.rb'])
-    subject.raw_migrations.should == ['2_name.rb']
+    names = [
+      '2_name.rb', #valid
+      'a_bad_name.rb', #invalid
+      '.#1_migration_a.rb', #invalid
+      '3_name.rb', #valid
+    ]
+    subject.raw_migrations(names)
+    subject.raw_migrations.should == ['2_name.rb', '3_name.rb']
   end
 
 end
